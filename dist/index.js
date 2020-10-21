9110feef42bf2c7ff8d58bcf1b7c84b99101d5e9
Mini Bear <pzqu@qq.com>
2020-10-21 13:25:01 +0800

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -860,6 +860,7 @@ module.exports = (function(e, t) {
         console.error(`Unable to get gist\n${e}`);
       }
       const r = [];
+      console.Console(e.data)
       for (let t = 0; t < Math.min(e.data.languages.length, 5); t++) {
         const n = e.data.languages[t];
         const { name: i, percent: s, text: o } = n;
